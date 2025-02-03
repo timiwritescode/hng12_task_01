6be59e6e10e07ac59ebbb8d9c27fe01abf3ff00b
timiwritescode <odebodezion@gmail.com>
2025-02-03 01:58:41 +0100

    add functionality for checking if number is perfect

--- src/numbers/dto/response.dto.ts
+++ src/numbers/dto/response.dto.ts
@@ -5,27 +5,32 @@ export class ResponseDto {
     is_prime: boolean;
     is_perfect: boolean;
     properties: string[];
-    // digit_sum: number;
+    digit_sum: number;
     fun_fact: string;
 
     private constructor(
         num: number, 
         is_prime: boolean,
+        is_perfect: boolean,
         fun_fact: string,
-        numberProperties: string[]) {
+        numberProperties: string[],
+        digitSum: number) {
         this.number = num;
         this.is_prime = is_prime;
+        this.is_perfect = is_perfect;
         this.properties = numberProperties
 
-        // this.digit_sum = num.getNumDigitSum();
+        this.digit_sum = digitSum;
         this.fun_fact = fun_fact
     }
 
     static async create(num: NumberInput) {
         const number = num.getNum();
         const is_prime = num.isNumPrime();
+        const is_perfect = num.isNumPerfect()
         const fun_fact = await num.getNumFunFact();
         const numProperties = num.getProperties();
-        return new ResponseDto(number, is_prime, fun_fact, numProperties)
+        const digitSum = num.getDigitSum();
+        return new ResponseDto(number, is_prime, is_perfect, fun_fact, numProperties, digitSum)
     }
 }
--- src/numbers/number.ts
+++ src/numbers/number.ts
@@ -28,20 +28,41 @@ export class NumberInput {
     }
 
     isNumPerfect(): boolean {
-        return;
+        let sumOfFactors = 0
+        for (let i = 0; i < this.num; i++) {
+            if (i > this.num / 2) {
+                break;
+            }
+            if (this.num % i == 0) {
+                sumOfFactors += i
+            }
+        }
+
+        return sumOfFactors === this.num;
     }
 
-    getNumDigitSum(): number{
-        return
-    }
 
     getProperties() {
         const numberProperties = []
         const oddOrEven = NumberInput.isNumberEven(this.num) ? "even" : "odd";
-
+        
+        if (NumberInput.isArmstrongNumber(this.num)) {
+            numberProperties.push("armstrong")
+        }
         numberProperties.push(oddOrEven)
         return numberProperties;
 
+    }
+
+    getDigitSum(): number{
+        const stringifiedDigits = "" + this.num
+        let totalNumber = 0;
+
+        for (let digit of stringifiedDigits) {
+            totalNumber += Number.parseInt(digit)
+        }
+
+        return totalNumber;
     }
 
     async getNumFunFact(): Promise<string> {
@@ -52,4 +73,15 @@ export class NumberInput {
     static isNumberEven(number: number): boolean {
         return number < 0 ? Math.abs(number) % 2 == 0 : number % 2 == 0
     }
-}
+
+    static isArmstrongNumber(number: number): boolean {
+        const stringifiedNumber = "" + number;
+        const power = stringifiedNumber.length;
+        let totalNumber = 0;
+        for (let stringNum of stringifiedNumber) {
+            totalNumber += parseInt(stringNum) ** power
+
+        }
+        return totalNumber == number;
+    }
+ }
