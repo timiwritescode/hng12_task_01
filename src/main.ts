0cdaa9324d8889f440479f769632ddafd324e15b
timiwritescode <odebodezion@gmail.com>
2025-02-17 23:00:24 +0100

    test integrations

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // app.setGlobalPrefix()
-  app.enableCors({
-    origin: '*',
-    allowedHeaders: '*',
-    methods: '*',
-    credentials: true
-  })
+  // app.enableCors({
+  //   origin: '*',
+  //   allowedHeaders: '*',
+  //   methods: '*',
+  //   credentials: true
+  // })
   await app.listen(process.env.PORT || 3000);
 
 }
